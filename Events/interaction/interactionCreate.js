334179b0201f7d79dd6b66397bc6bd82f3f046a9
imadragonsz <mittchelk@gmail.com>
2022-02-13 00:32:01 +0100

    hyel

---
+++ Events/interaction/interactionCreate.js
@@ -0,0 +1,26 @@
+const { Client, CommandInteraction, MessageEmbed } = require('discord.js');
+const { execute } = require('../client/ready');
+
+module.exports = {
+	name: 'interactionCreate',
+	/**
+     * 
+     * @param {CommandInteraction} interaction 
+     * @param {Client} client 
+     */
+	async execute(interaction, client) {
+		if (interaction.isCommand()) {
+			const command = client.commands.get(interaction.commandName);
+			if (!command)
+				return (
+					interaction.reply({
+						embeds: [
+							new MessageEmbed().setColor('RED').setDescription('⛔ An error has occured while running this command.')
+						]
+					}) && client.command.delete(interaction.commandName)
+				);
+
+			command.execute(interaction, client);
+		}
+	}
+};
--- handlers/commands.js
+++ handlers/commands.js
@@ -1,4 +1,4 @@
-const { perms } = require('../validation/permissions');
+const { Perms } = require('../validation/permissions');
 const { client } = require('discord.js');
 const { promisify } = require('util');
 const { glob } = require('glob');
@@ -19,8 +19,44 @@ module.exports = async (client) => {
 		if (!command.description) return table.addRow(command.name, '🔸 FAILED', 'Missing a description.');
 
 		if (command.permission) {
-			if (perms.includes(command.permission)) command.defaultPermission = false;
+			if (Perms.includes(command.permission)) command.defaultPermission = false;
 			else return table.addRow(command.name, '🔸 FAILED', 'Permission is invalid.');
 		}
+
+		client.commands.set(command.name, command);
+		commandsArray.push(command);
+
+		await table.addRow(command.name, '🔹 SUCCESFUL');
+	});
+
+	console.log(table.toString());
+
+	//persmissions check //
+
+	client.on('ready', async () => {
+		const MainGuild = await client.guilds.cache.get('857729547985879081');
+
+		MainGuild.commands.set(commandsArray).then(async (command) => {
+			const Roles = (commandName) => {
+				const cmdPerms = commandsArray.find((c) => c.name === commandName).permission;
+				if (!cmdPerms) return null;
+
+				return MainGuild.roles.cache.filter((r) => r.permissions.has(cmdPerms));
+			};
+
+			const help = command.reduce((accumulator, r) => {
+				const roles = Roles(r.name);
+				if (!roles) return accumulator;
+
+				const permissions = roles.reduce((a, r) => {
+					return [ ...a, { id: r.id, type: 'ROLE', permission: true } ];
+				});
+				[];
+				return [ ...accumulator, { id: r.id, permissions } ];
+			});
+			[];
+
+			await MainGuild.commands.permissions.set({ help });
+		});
 	});
 };
