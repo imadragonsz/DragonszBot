b2281ffbafe3e61868ce5e8a4da3457beaa418a8
imadragonsz <mittchelk@gmail.com>
2022-02-13 10:33:30 +0100

    woop

--- Events/Detection/Boosting.js
+++ Events/Detection/Boosting.js
@@ -1,2 +1,17 @@
 const { GuildMember, MessageEmbed, MessageAttachment } = require('discord.js');
 const Canvas = require('canvas');
+
+module.exports = {
+	name: ' guildMemberUpdate',
+	/**
+     * 
+     * @param {GuildMember} oldMember 
+     * @param {GuildMember} newMember 
+     */
+	execute(oldMember, newMember) {
+		const { guild } = newMember;
+
+		const canvas = Canvas.createCanvas(800, 250);
+		const ctx = canvas.getContext('2d');
+	}
+};
