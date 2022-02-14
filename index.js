29f6dd6db364e5852ac4068b14e2cbc38c82f20d
imadragonsz <mittchelk@gmail.com>
2022-02-14 21:50:07 +0100

    si

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const { Token } = require('./Structures/token.json');
-
 //constants
-const { config, prefix } = require('./Structures/config.json');
 const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: 14023 });
+const { Token } = require('./Structures/token.json');
+
 const { promisify } = require('util');
 const { glob } = require('glob');
 const PG = promisify(glob);
 const Ascii = require('ascii-table');
-
 client.commands = new Collection();
 
 [ 'Events', 'Commands' ].forEach((handler) => {
