653de745b8ecffa90be2e455663e38a7f7eb501d
imadragonsz <mittchelk@gmail.com>
2022-02-16 00:14:53 +0100

    sa

---
+++ commands/System/help.js
@@ -0,0 +1,40 @@
+const { CommandInteraction, MessageEmbed } = require('discord.js');
+
+module.exports = {
+	name: 'help',
+	description: 'shows the commands available',
+	permissions: 'ADMINISTRATOR',
+	/**
+     * 
+     * @param {CommandInteraction} interaction 
+     */
+	async execute(interaction) {
+		const User = interaction.member;
+		const Response = new MessageEmbed()
+			.setColor('RED')
+			.setAuthor({
+				name: `${User.user.username}`,
+				iconURL: `${User.user.avatarURL({ dynamic: true, size: 512 })}`
+			})
+			.addFields(
+				{
+					name: 'Testing Commands',
+					value: '/emitt: emitts a different command'
+				},
+				{
+					name: 'mod commands',
+					value: '/clear: clears specified amount of messages of selected user'
+				},
+				{
+					name: 'System commands',
+					value: `/help: shows this menu\n/status: shows the bot status\n/suggest: use this command to make suggestions`
+				},
+				{
+					name: 'Apps',
+					value: 'userinfo: right click on a user and use the userinfo app to show their info'
+				}
+			);
+
+		interaction.reply({ embeds: [ Response ], ephemeral: true });
+	}
+};
